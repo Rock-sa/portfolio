67150874304cea3ed381dcdb739bd5fae57aace5
Rock-sa <shakshi103103@gmail.com>
2021-05-16 15:43:18 +0530

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+#create blog
+class Blog(models.Model):
+    title = models.CharField(max_length=255) #title
+    pub_date =  models.DateTimeField() #pubdate
+    body = models.TextField() #body
+    image = models.ImageField(upload_to='images/')#image
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,12 +25,14 @@ SECRET_KEY = '1$u0xwdyxt^$)a8q9^8-suv=6^fj$r0vq8a(4qzv0j#2ukyve&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
